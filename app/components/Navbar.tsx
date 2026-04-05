ff36896535b1552f0177e2044b09ca48077770aa
Thomas Collinson <tomcollinson14@gmail.com>
2026-04-05 11:17:40 +0100

    Logo Update: Double size to 180px and replaced with provided sunset landscape image

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     <nav className="navbar">
       <Link href="/" className="logo-link">
         <img
-          src="/assets/logo_v3.png?v=2"
+          src="/assets/final_logo.jpg?v=1"
           alt="Craven Calm Logo"
           className="logo-img"
           onError={(e) => {
